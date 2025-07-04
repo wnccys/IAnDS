8ec8ce72d7c5b358cc92ea409ccbdabc94c1daa0
wnccys <ipooo346@gmail.com>
2025-07-04 18:43:06 -0300

    feat: adjust shift to correct swap and set new Item repr

--- greedy_bag/src/main.rs
+++ greedy_bag/src/main.rs
@@ -1,79 +1,95 @@
 /// An optimal solution of a basic greedy algorithm
+/// 
 
+#[derive(Debug)]
 struct Item {
+    id: u16,
     scr: f32,
     wgt: f32,
 }
+
+impl Item {
+    fn as_ratio(&self) {
+        println!("{}", self.scr / self.wgt);
+    }
+}
+
 
 /// Default bag size
 /// 
 const SIZE: usize = 5;
 
+/// Weight limit of bag
+/// 
+const LIMIT: u16 = 25;
+
 fn main() {
-    let bag: [Item; SIZE] = [
-        Item { scr: 20.0, wgt: 10.0 },
-        Item { scr: 30.0, wgt: 10.0 },
-        Item { scr: 20.0, wgt: 40.0 },
-        Item { scr: 15.0, wgt: 55.0 },
-        Item { scr: 12.0, wgt: 25.0 },
+    let mut bag: [Item; SIZE] = [
+        Item { scr: 20.0, wgt: 10.0, id: 1 },
+        Item { scr: 30.0, wgt: 10.0, id: 2 },
+        Item { scr: 20.0, wgt: 40.0, id: 3 },
+        Item { scr: 15.0, wgt: 55.0, id: 4 },
+        Item { scr: 12.0, wgt: 25.0, id: 5 },
     ];
-    let mut ratios = calc_ratio(&bag);
+    quick_sort(&mut bag);
 
-    println!("{:?}", greed(quick_sort(&mut ratios[..])));
-}
-
-fn calc_ratio(bag: &[Item; SIZE]) -> [f32; SIZE] {
-    let mut ratios: [f32; SIZE] = [0.0; SIZE];
-
-    for (idx, it) in bag.iter().enumerate() {
-        ratios[idx] = it.scr / it.wgt
+    for n in bag.iter() {
+        println!("{:?}", n.scr / n.wgt);
     }
 
-    ratios
+    println!("{:?}", greed(&mut bag));
 }
 
-fn quick_sort(ratio_slice: &mut [f32]) -> &mut [f32] {
+fn quick_sort(bag: &mut [Item]) {
     /* base case */
-    if ratio_slice.len() < 2 { return ratio_slice };
+    if bag.len() < 2 { return };
 
     let mut piv_pos = 0;
     let mut curr = 1;
 
-    // O(n + (piv_pos..curr * n)) iteration
-    while curr < ratio_slice.len() {
-        let piv = ratio_slice[piv_pos];
+    // ~ O(n + ((piv_pos..curr * n) / 2)) iteration
+    while curr < bag.len() {
+        dbg!(bag[curr].scr / bag[curr].wgt, bag[piv_pos].scr / bag[piv_pos].wgt);
 
-        match curr {
-            _ if ratio_slice[curr] <= piv => {
-                l_shift(ratio_slice, piv, &mut piv_pos, curr);
-            }
-            _ => ()
+        if bag[curr].scr / bag[curr].wgt <= bag[piv_pos].scr / bag[piv_pos].wgt {
+            l_shift(bag, &mut curr);
+            piv_pos += 1;
         }
 
         curr += 1;
     }
-    dbg!(&ratio_slice);
 
-    quick_sort(&mut ratio_slice[..piv_pos]);
-    quick_sort(&mut ratio_slice[piv_pos+1..]);
-
-    ratio_slice
+    quick_sort(&mut bag[..piv_pos]);
+    quick_sort(&mut bag[piv_pos+1..]);
 }
 
-/// Move elements starting from the left of slice
+/// Move elements one by one based on pivot position
 /// 
-fn l_shift<'a>(slc: &'a mut [f32], mut piv: f32, piv_pos: &mut usize, t_idx: usize) -> &'a mut [f32] {
-    slc[*piv_pos] = slc[t_idx];
-    *piv_pos += 1;
+fn l_shift<'a>(bag: &'a mut [Item], t_idx: &usize) {
+    for i in 0..*t_idx {
+        bag.swap(*t_idx-i-1, t_idx-i);
 
-    for i in *piv_pos..=t_idx {
-        piv = std::mem::replace(&mut slc[i], piv);
+        for it in bag.iter() {
+            it.as_ratio();
+        }
     };
-
-    slc
 }
 
-fn greed(n: &mut [f32]) -> &mut [f32] {
-    dbg!(n);
-    todo!();
+fn greed(bag: &mut [Item]) -> &mut [f32] {
+    let mut curr_cost = 0;
+
+    loop {
+        for item in bag.iter() {
+            if curr_cost > LIMIT { break }
+
+            match curr_cost {
+                n if curr_cost + n < LIMIT => { curr_cost += n }
+                n if curr_cost < LIMIT && curr_cost + n > LIMIT => {
+
+                }
+                _ => ()
+            }
+
+        }
+    }
 }
