e9a31c1ea1d4516a2926c59ef4679abe4645d83a
wnccys <ipooo346@gmail.com>
2025-10-02 07:46:46 -0300

    feat: set base structs

--- b_plus_tree/src/main.rs
+++ b_plus_tree/src/main.rs
@@ -1,3 +1,24 @@
+const ORDER: usize = 4;
+const MAX_LEAF_CAP: usize = ORDER - 1;
+
+trait Tree {
+    fn insert(id: usize, name: String) -> ();
+}
+
+struct Root([Option<Box<dyn Node>>; ORDER]);
+
+struct Internal<'a> {
+    left: &'a dyn Node,
+    right: &'a dyn Node,
+}
+
+struct Leaf {
+    key: usize,
+    value: String,
+}
+
+trait Node {}
+
 fn main() {
     println!("Hello, world!");
 }
