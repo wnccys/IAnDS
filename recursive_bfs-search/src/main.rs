540f920c411877dc8e6c44d1e843f2c8dd5f331e
wnccys <ipooo346@gmail.com>
2025-10-09 18:51:36 -0300

    feat: finish implementation

--- recursive_bfs-search/src/main.rs
+++ recursive_bfs-search/src/main.rs
@@ -1,33 +1,101 @@
-use std::collections::HashMap;
+use std::collections::VecDeque;
 
-/// Breadth First Search implementation
+/// Minimal BFS graph algorithm implementation
+/// 
+/// This searches an certain name and uses an Id in order to identify each Node (only miscellaneouslly);
+/// 
+/// The mocks below
+
+#[derive(Debug)]
+pub struct Node<'a> {
+    pub id: usize,
+    pub name: String,
+    pub neighbors: Option<Vec<&'a Node<'a>>>
+}
+
+impl<'a> Node<'a> {
+    fn new(id: usize, name: String, neighbors: Option<Vec<&'a Node>>) -> Self {
+        Node {
+            id,
+            name,
+            neighbors
+        }
+    }
+}
+
+// test for n4, the only pointing to n9
+// test for n8 search, it is the only order 4 search (need layer 3 to be found);
+
+fn main() {
+    let args: Vec<_> = std::env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: [PROGRAM] -- [Search Name]");
+    }
+    
+    // l3 relations
+    let n9 = Node::new(9, "Poseidon".to_string(), None);
+    let n8 = Node::new(8, "Gaea".to_string(), None);
+    let n7 = Node::new(7, "Chaos".to_string(), Some(vec![&n8]));
+    let n6 = Node::new(6, "Uranus".to_string(), None);
+
+    // l2 relations
+    let n5 = Node::new(5, "Ares".to_string(), Some(vec![&n7, &n6]));
+    let n4 = Node::new(4, "Atenas".to_string(), Some(vec![&n9]));
+    let n3 = Node::new(3, "Apollo".to_string(), Some(vec![&n6]));
+
+    // l1 relations
+    let n2 = Node::new(2, "Hera".to_string(), Some(vec![&n5]));
+    let n1 = Node::new(1, "Zeus".to_string(), Some(vec![&n3, &n4]));
+
+    let root = Node::new(0, "Fate".to_string(), Some(vec![&n1, &n2]));
+    
+    let mut queue = VecDeque::new();
+    queue.push_front(&root);
+
+    let mut processed = vec![];
+
+    let result = bfs_search(&args[1][..], &mut queue, &mut processed);
+
+    match result {
+        Some(target) => {
+            println!("Found: {:?}", target);
+        }
+        None => println!("Could not find any item named {}!", &args[1][..])
+    }
+}
+
+/// Breadth-First Search impl
 /// 
 pub fn bfs_search<'a>(
-    graph: &'a HashMap<&str, Vec<&str>>,
-    target: &str,
-    queue: &mut Vec<&'a str>,
-    processed: &mut Vec<&'a str>
-) -> Option<&'a str> {
+    target: &'_ str,
+    queue: &'a mut VecDeque<&'_ Node<'_>>,
+    processed: &'_ mut Vec<usize>
+) -> Option<&'a Node<'a>> {
     // Base case
-    if queue.len() == 0 { return None };
+    if queue.is_empty() { return None };
 
-    let person = queue.pop().unwrap();
-    processed.push(person);
+    // Remove [Self] Node from start of processing queue HERE -> [..]
+    let node = queue.pop_front().unwrap();
 
-    let friends = match graph.get(person) {
-        Some(vec) => vec,
-        _ => &Vec::new(),
-    };
+    // Names comparison are normalized, so poseidon matches with Poseidon
+    if node.name.to_lowercase() == target.to_lowercase() { return Some(node); }
 
-    // Check if friends are present and are no in processed
-    if friends.contains(&target) {
-        return Some(person);
+    // Set neighbors to end of queue
+    if node.neighbors.is_some() {
+        // Filters processed ones
+        for neighbor in node.neighbors.as_ref().unwrap()
+            .iter()
+            .filter(|node| !processed.contains(&node.id)) 
+        {
+            // Add neighbor to end of queue HERE [..] <-
+            queue.push_back(neighbor);
+        }
+
+        // Set current node as processed
+        processed.push(node.id);
     }
 
-    // Add new friends to queue
-    for &person in friends {
-        if !processed.contains(&person) { queue.push(person) }
-    }
-
-    bfs_search(graph, target, queue, processed)
+    // Re-init search, until find a name or falling back to None, when no entry was found
+    return bfs_search(target, queue, processed);
 }
